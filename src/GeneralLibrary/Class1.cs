00caba0e895f0cf0bf9b06798d896ae8b016d3b3
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-10-17 14:13:06 +0200

    Add vulnerability

--- src/GeneralLibrary/Class1.cs
+++ src/GeneralLibrary/Class1.cs
@@ -4,5 +4,11 @@ namespace GeneralLibrary
 {
     public class Class1
     {
+        public void DoSomething()
+        {
+            // ...
+            Console.WriteLine("so far, so good...");
+            // ...
+        }
     }
 }
